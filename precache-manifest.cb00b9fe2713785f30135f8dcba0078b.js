eabaebc0a0fdb1615f65ad384731bd5b92fe270a
raison00 <felicia.artist@gmail.com>
2020-08-17 10:59:56 -0700

    Updates

---
+++ precache-manifest.cb00b9fe2713785f30135f8dcba0078b.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c84540ffd11789d92c083c42aa1d07d",
+    "url": "/devtools-ui-kit-2020/index.html"
+  },
+  {
+    "revision": "cef3f207f6f0f6dcf2c9",
+    "url": "/devtools-ui-kit-2020/static/css/main.209937d9.chunk.css"
+  },
+  {
+    "revision": "cf411239a06f78aa3f86",
+    "url": "/devtools-ui-kit-2020/static/js/2.dfa9d7c0.chunk.js"
+  },
+  {
+    "revision": "24f3b8174660712e2ea3ee62d07568c6",
+    "url": "/devtools-ui-kit-2020/static/js/2.dfa9d7c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cef3f207f6f0f6dcf2c9",
+    "url": "/devtools-ui-kit-2020/static/js/main.647ea94f.chunk.js"
+  },
+  {
+    "revision": "025dda9b14fd964692d5",
+    "url": "/devtools-ui-kit-2020/static/js/runtime-main.da9c4797.js"
+  },
+  {
+    "revision": "01899307116a937c66a0eb4b3927b631",
+    "url": "/devtools-ui-kit-2020/static/media/blob.01899307.png"
+  },
+  {
+    "revision": "8d581ffb03c56a0106abc6b330b449d5",
+    "url": "/devtools-ui-kit-2020/static/media/cercuri.8d581ffb.png"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/devtools-ui-kit-2020/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "3c22111465e24c64da6e597a4be02c49",
+    "url": "/devtools-ui-kit-2020/static/media/flutter-blue-green.3c221114.png"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/devtools-ui-kit-2020/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/devtools-ui-kit-2020/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "c085e83e15808eff561f13f30ce937c2",
+    "url": "/devtools-ui-kit-2020/static/media/landing-page.c085e83e.png"
+  },
+  {
+    "revision": "f4f3bd7990601e8642336af37adbd583",
+    "url": "/devtools-ui-kit-2020/static/media/le-flutter-inspect-lposition-left.f4f3bd79.png"
+  },
+  {
+    "revision": "fb76b97a83dde344d064114c8351603e",
+    "url": "/devtools-ui-kit-2020/static/media/le-icon.fb76b97a.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/devtools-ui-kit-2020/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/devtools-ui-kit-2020/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/devtools-ui-kit-2020/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/devtools-ui-kit-2020/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "b1e01ebbb0d098277dcc97759bc3f402",
+    "url": "/devtools-ui-kit-2020/static/media/path1.b1e01ebb.png"
+  },
+  {
+    "revision": "43ff45c5dc1f65726b86fc75ea6cabc0",
+    "url": "/devtools-ui-kit-2020/static/media/path2.43ff45c5.png"
+  },
+  {
+    "revision": "a70167ea1491987c50049961a9e2e45f",
+    "url": "/devtools-ui-kit-2020/static/media/path3.a70167ea.png"
+  },
+  {
+    "revision": "e37326c7e0fd9826c1a35b231340f58b",
+    "url": "/devtools-ui-kit-2020/static/media/path4.e37326c7.png"
+  },
+  {
+    "revision": "ceb241f05718fb1311f8810eea959803",
+    "url": "/devtools-ui-kit-2020/static/media/path5.ceb241f0.png"
+  },
+  {
+    "revision": "03ea2a0ad05689cebb5a9314a53b8966",
+    "url": "/devtools-ui-kit-2020/static/media/patrat.03ea2a0a.png"
+  },
+  {
+    "revision": "0ace53b15c85e534a867e3b9480d0e68",
+    "url": "/devtools-ui-kit-2020/static/media/phone.0ace53b1.png"
+  },
+  {
+    "revision": "2515c8e5bccbb8334071a0cce1bb2540",
+    "url": "/devtools-ui-kit-2020/static/media/profile-page.2515c8e5.png"
+  },
+  {
+    "revision": "d496fc61e15919e3cbafeddf69afa274",
+    "url": "/devtools-ui-kit-2020/static/media/square1.d496fc61.png"
+  },
+  {
+    "revision": "2eb753e7af5d4f836aeccdead0e1afb5",
+    "url": "/devtools-ui-kit-2020/static/media/square2.2eb753e7.png"
+  },
+  {
+    "revision": "6ec041806d452099049a3527bb3b0d81",
+    "url": "/devtools-ui-kit-2020/static/media/square3.6ec04180.png"
+  },
+  {
+    "revision": "5ac16c12c223ae3525784888d93a8198",
+    "url": "/devtools-ui-kit-2020/static/media/square4.5ac16c12.png"
+  },
+  {
+    "revision": "4964a9d0e0dc7470288f54affecf1252",
+    "url": "/devtools-ui-kit-2020/static/media/square5.4964a9d0.png"
+  },
+  {
+    "revision": "4a5b4fa6b2d7f86567416858bb3cb1bf",
+    "url": "/devtools-ui-kit-2020/static/media/square6.4a5b4fa6.png"
+  },
+  {
+    "revision": "11fa5f73cb719f0f1a38e02e47bdc843",
+    "url": "/devtools-ui-kit-2020/static/media/waves.11fa5f73.png"
+  }
+]);
